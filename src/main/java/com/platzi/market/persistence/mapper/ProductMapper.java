e9041447653a93521cc7b8ed7fd222b27f279d0a
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 12:36:17 -0500

    Class 22: MapStruct

---
+++ src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Product;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProduct", target = "productId"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "idCategory", target = "categoryId"),
+            @Mapping(source = "price", target = "price"),
+            @Mapping(source = "stockQuantity", target = "stock"),
+            @Mapping(source = "state", target = "active"),
+            @Mapping(source = "category", target = "category"),
+    })
+    Product toProduct(com.platzi.market.persistence.entity.Product product);
+    List<Product> toProducts(List<com.platzi.market.persistence.entity.Product> products);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "barCode", ignore = true)
+    com.platzi.market.persistence.entity.Product toProductE(Product product);
+}
