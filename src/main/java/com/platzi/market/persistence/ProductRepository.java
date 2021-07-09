a355a48d4f4beffbb7481548fc9906bf3b92298c
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 12:57:04 -0500

    Class 23: Orient our repository to domain terms - ProductRepository

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -1,38 +1,49 @@
 package com.platzi.market.persistence;
 
+import com.platzi.market.domain.Product;
 import com.platzi.market.persistence.crud.ProductCrudRepository;
-import com.platzi.market.persistence.entity.Product;
+import com.platzi.market.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements com.platzi.market.domain.repository.ProductRepository {
     private ProductCrudRepository productCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Product> getAll(){
-        return (List<Product>) productCrudRepository.findAll();
+    @Override
+    public List<Product> getAll() {
+        List<com.platzi.market.persistence.entity.Product> products = (List<com.platzi.market.persistence.entity.Product>) productCrudRepository.findAll();
+        return mapper.toProducts(products);
     }
 
-    public List<Product> getByCategory(int idCategory){
-        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<com.platzi.market.persistence.entity.Product> products = productCrudRepository.findByIdCategoryOrderByNameAsc(categoryId);
+        return Optional.of(mapper.toProducts(products));
     }
 
-    public Optional<List<Product>> getScarce(int quantity){
-        return productCrudRepository.findByStockQuantityLessThanAndState(quantity, true);
+    @Override
+    public Optional<List<Product>> getScarceProducts(int quantity) {
+        Optional<List<com.platzi.market.persistence.entity.Product>> products = productCrudRepository.findByStockQuantityLessThanAndState(quantity, true);
+        return products.map(prods -> mapper.toProducts(prods));
     }
 
-    public Optional<Product> getProduct(int idProducto) {
-        return productCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productCrudRepository.findById(productId).map(productE -> mapper.toProduct(productE));
     }
 
+    @Override
     public Product save(Product product) {
-        return productCrudRepository.save(product);
+        com.platzi.market.persistence.entity.Product productE = mapper.toProductE(product);
+        return mapper.toProduct(productCrudRepository.save(productE));
     }
 
-    public void delete(int idProduct) {
-        productCrudRepository.deleteById(idProduct);
+    @Override
+    public void delete(int productId) {
+        productCrudRepository.deleteById(productId);
     }
 }
