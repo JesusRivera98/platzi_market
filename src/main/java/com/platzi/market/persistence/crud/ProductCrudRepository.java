a5dc0e6bf7a8bfcf530a27806d34e8aeda5c6e2f
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 11:23:47 -0500

    Class 19: Add Native query

--- src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
@@ -1,7 +1,13 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+    @Query(value = "SELECT * FROM products WHERE id_category = ?", nativeQuery = true)
+    List<Product> findByIdCategory(int idCategory);
+
 }
