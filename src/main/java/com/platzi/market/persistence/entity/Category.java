506d1b3be6d85fd5e2d11bb5d56a8b9c5b03889d
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 10:41:58 -0500

    Class 16: Create Category and Client Classes

---
+++ src/main/java/com/platzi/market/persistence/entity/Category.java
@@ -0,0 +1,40 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private Integer idCategory;
+
+    private String description;
+    private Boolean state;
+
+    public Integer getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(Integer idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+}
