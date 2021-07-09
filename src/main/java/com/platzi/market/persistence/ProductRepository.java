8c676884bf0cbf9bba1aa22c3c51a271a31cca42
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 12:06:44 -0500

    Class 20: @Repository

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,10 +2,13 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -17,8 +20,19 @@ public class ProductRepository {
         return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
     }
 
-    public List<Product> getScarce(int quantity){
-        return productCrudRepository.findByStockQuantityLessThanAndState(quantity);
+    public Optional<List<Product>> getScarce(int quantity){
+        return productCrudRepository.findByStockQuantityLessThanAndState(quantity, true);
+    }
 
+    public Optional<Product> getProduct(int idProducto) {
+        return productCrudRepository.findById(idProducto);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
