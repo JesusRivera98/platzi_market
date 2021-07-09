262867964e1ade1662c97781c5b2e57df144f132
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 14:33:52 -0500

    Class 25: Annotation @Service - Delete with if

---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.persistence.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getBayCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+
+   public Product save(Product product){
+        return productRepository.save(product);
+   }
+
+   public boolean delete(int productId){
+        if(getProduct(productId).isPresent()){
+            productRepository.delete(productId);
+            return true;
+        }
+        return false;
+   }
+
+}
