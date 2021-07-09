ad39ce3c6305c52ebe2ea60c99fc418525fef6e0
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 11:24:57 -0500

    Class 19: Remove Native query

--- src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
-    @Query(value = "SELECT * FROM products WHERE id_category = ?", nativeQuery = true)
     List<Product> getByCategory(int idCategory);
-
 }
