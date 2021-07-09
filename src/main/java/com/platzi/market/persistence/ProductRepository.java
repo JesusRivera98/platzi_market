fca58d69575fb634bbb5591f03d89bf216015297
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-09 11:09:15 -0500

    Class 18: Using the CrudRepository interface

---
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.platzi.market.persistence;
+
+import com.platzi.market.persistence.crud.ProductCrudRepository;
+import com.platzi.market.persistence.entity.Product;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+public class ProductRepository {
+    private ProductCrudRepository productCrudRepository;
+
+    public List<Product> getAll(){
+        return (List<Product>) productCrudRepository.findAll();
+    }
+}
